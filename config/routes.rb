caf39b50252637c0aeec5015b3caad75cb900bb8
Ashley <ashleynjeri7@gmail.com>
2022-11-27 19:54:00 +0300

    Rails Resource Routing: Destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create, :update]
+  #resources :birds, only: [:index, :show, :create, :update, :destroy]
+  resources :birds # since we're using all 5 RESTful routes
+  
   patch "/birds/:id/like", to: "birds#increment_likes"
 end
